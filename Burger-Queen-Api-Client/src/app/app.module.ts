58f6ba49e69820c6a5d09d065e18ab12c4dd4f4a
indira Davoin <lunarte.mix@gmail.com>
2023-06-11 18:50:04 -0400

    ADD: Evolucion Login

--- Burger-Queen-Api-Client/src/app/app.module.ts
+++ Burger-Queen-Api-Client/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from '@shared/shared.module';
 import { LoginPageComponent } from './views/login/login-page/login-page.component';
 
-LoginPageComponent
+
 @NgModule({
   declarations: [ //TODO: Declaraciones, componentes, directivas, pipes
     AppComponent,
@@ -16,6 +17,7 @@ LoginPageComponent
     BrowserModule,
     AppRoutingModule,
     SharedModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
