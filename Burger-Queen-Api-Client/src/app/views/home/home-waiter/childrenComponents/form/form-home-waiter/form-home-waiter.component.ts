ceeb3c78bb48b3fcaccba009cf3522adc78efa94
LadyDi3103 <chinamy2004@gmail.com>
2023-06-21 23:04:26 -0500

    ADD: Reactive Form in the waiter login

--- Burger-Queen-Api-Client/src/app/views/home/home-waiter/childrenComponents/form/form-home-waiter/form-home-waiter.component.ts
+++ Burger-Queen-Api-Client/src/app/views/home/home-waiter/childrenComponents/form/form-home-waiter/form-home-waiter.component.ts
@@ -39,50 +39,63 @@ export class FormHomeWaiterComponent {
   // clientsName: string;
   
   products: any[] = []; // AQUÍ DEBERÍA PUSHEARSE LOS PRODUCTOS AL SELECCIONAR
+  // numOrder: new FormControl('0000000'),
   
   // Formulario REACTIVO ****************************************************
-  form:FormGroup = new FormGroup({
+  form = new FormGroup({
     clientsName: new FormControl(''),
-    numOrder: new FormControl('0000000'),
-    numTable: new FormControl(''),
+    numTable: new FormControl(1),
     total: new FormControl(''),
-    totalCost: new FormControl(''),
-    counter: new FormControl(1),
+    // totalCost: new FormControl(''),
   });
-  numOrder: number = 1;
-  
+  //form va toda la data que se ingresa en un input
+  counter: number = 1;
+  numOrder: string = '0000001';
+  // clientsName: string = '';
+  // numTable: number = 0;
+  // totalCost: number = 0;
+  // total: number = 0;
+  // counter: number = 1;
 //*********************************************************
+//cada producto debería tener su propio counter
+increase(value:number):void {
+  this.counter += value;
+  // console.log(this.counter);
+  // debe incrementar en el contador de precios unitarios la cantidad del item.
+}
 
-  // increase(value:number):void {
-  //   this.counter += value;
-  //   // console.log(this.counter);
-  //   // debe incrementar en el contador de precios unitarios la cantidad del item.
-  // }
+decrease(value:number):void {
+  if (this.counter > 0) {
+    this.counter -= value;
+    // console.log(this.counter);
+    // debe decrecrementar en el contador de precios unitarios la cantidad del item.
+  }
+}
 
-  // decrease(value:number):void {
-  //   if (this.counter > 0) {
-  //     this.counter -= value;
-  //     // console.log(this.counter);
-  //     // debe decrecrementar en el contador de precios unitarios la cantidad del item.
-  //   }
-  // }
-  // incrementOrderNumber(): void {
-  //   let number = parseInt(this.numOrder);
-  //   number++;
-  //   this.numOrder = number.toString();
-  //   // .padStart(7, '0');
-  // }
+  incrementOrderNumber(): void {
+    // let number = parseInt(this.numOrder);
+    // number++;
+    // this.numOrder = number.toString();
+    // .padStart(7, '0');
+  }
   // decrementOrderNumber() {
   //   let number = parseInt(this.numOrder);
   //   number--;
   //   this.numOrder = number.toString();
   //   // .padStart(7, '0');
-  // }
-
+  // // }
+  // addProduct(): void {
+  //   if (this.selectedProduct) {
+  //     this.selectedProducts.push(this.selectedProduct);
+  //     this.totalCost += this.selectedProduct.price;
+  //     this.selectedProduct = null;
+  //     console.log(this.selectedProduct, this.totalCost, this.selectedProducts, 126666666 );
+      
+  //   }
 
   onSubmitDoThis() {
-    this.incrementOrderNumber()
-    console.log(this.selectedMenu, this.counter, this.numOrder, this.numTable, this.totalCost, this.total, this.numTable.valueOf, this.clientsName, this.clientsName, 49999)
+    // this.incrementOrderNumber()
+    // console.log(this.selectedMenu, this.counter, this.numOrder, this.numTable, this.totalCost, this.total, this.numTable.valueOf, this.clientsName, this.clientsName, 49999)
 
   }
   // calculateTotalCost():void{
@@ -92,32 +105,36 @@ export class FormHomeWaiterComponent {
   // {{totalCost}}
   // }
   // ---------- quitar una seleción de producto
-  // deselectProduct(): void {
-  //   this.selectedMenu = null;
-  //   // this.menuItem.breakfast.forEach((product)=>{
-  //   //   product.selected = false; // reinicia la selección del producto breakfast
-  //   // });
-  //   // this.menuItem.meals.forEach((product)=>{
-  //   //   product.selected = false; // reinicia la selección del producto meals
-  //   // })
-  // }
+  deselectProduct(): void {
+    // this.selectedMenu = null;
+    // this.menuItem.breakfast.forEach((product)=>{
+    //   product.selected = false; // reinicia la selección del producto breakfast
+    // });
+    // this.menuItem.meals.forEach((product)=>{
+    //   product.selected = false; // reinicia la selección del producto meals
+    // })
+  }
 
-  // cancelOrder(): void {
-  //   console.log('CANCEL ORDER2');
-  //   //reiniciar los valores relacionados con la orden
-  //   console.log(this.clientsName);
+  cancelOrder(): void {
+    this.form.reset({
+      numTable: 0,
+       
+    });
+    // console.log('CANCEL ORDER2');
+    // //reiniciar los valores relacionados con la orden
+    // console.log(this.clientsName);
     
-  //   this.clientsName = ' ';        //no reinicia el nombre del cliente
-  //   this.numOrder = this.decrementOrderNumber();
-  //   this.selectedMenu = ' ';
-  //   this.counter = 0;
-  //   this.numTable = 0;             //no reinicia
-  //   this.total = 0;
-  //   this.totalCost = 0;
+    // this.clientsName = ' ';        //no reinicia el nombre del cliente
+    // this.numOrder = this.decrementOrderNumber();
+    // this.selectedMenu = ' ';
+    // this.counter = 0;
+    // this.numTable = 0;             //no reinicia
+    // this.total = 0;
+    // this.totalCost = 0;
     
 
     // this.selectedProduct = null;
-    // //restaurar los valores predeterminados de los productos
-    // }
+    //restaurar los valores predeterminados de los productos
+    }
   }
 // }
