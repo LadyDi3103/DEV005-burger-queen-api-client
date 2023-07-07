164f58789925260aa321faf037417233d48a0482
LadyDi3103 <chinamy2004@gmail.com>
2023-07-07 16:47:26 -0500

    ADD: fn to get orders from the API

--- Burger-Queen-Api-Client/src/app/services/OrderService/order.service.ts
+++ Burger-Queen-Api-Client/src/app/services/OrderService/order.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { DataOrder } from 'src/app/interfaces/interfaces';
@@ -6,18 +6,27 @@ import { DataOrder } from 'src/app/interfaces/interfaces';
 @Injectable({
     providedIn: 'root'
 })
-
+//implements OnInit
 export class OrderService {
-constructor(private http: HttpClient) { }
-
-createOrder(orderData:DataOrder): Observable<any> {
+    constructor(private http: HttpClient) { }
+    //El WAITER llama a esta función para crear la orden
+    createOrder(orderData: DataOrder): Observable<any> {
         return this.http.post('http://localhost:8080/orders', {
             cliente: orderData.client,
             products: orderData.products,
             status: orderData.status,
-        }
-        )
+        })
     }
+    getOrders(): Observable<any> {
+        const headers = new HttpHeaders({
+            'accept': 'application/json',
+            'Authorization': 'Bearer ' + localStorage.getItem("accessToken")
+        })
+        return this.http.get<any>('http://localhost:8080/orders', { headers });
+
+    }
+
+
     // sendOrder(order:): Observable<any>{
     //   const token = localStorage.getItem("accessToken");
     //   const headers = new HttpHeaders({
