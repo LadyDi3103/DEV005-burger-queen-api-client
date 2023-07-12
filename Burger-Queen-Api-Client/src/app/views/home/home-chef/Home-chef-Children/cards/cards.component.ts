f69e10ddb2c6f16f25ad7368970bfbf0e71d5594
LadyDi3103 <chinamy2004@gmail.com>
2023-07-12 01:42:37 -0500

    ADD: @Output  to emit a value in cardsComponent

--- Burger-Queen-Api-Client/src/app/views/home/home-chef/Home-chef-Children/cards/cards.component.ts
+++ Burger-Queen-Api-Client/src/app/views/home/home-chef/Home-chef-Children/cards/cards.component.ts
@@ -6,15 +6,24 @@ import { DataOrder } from 'src/app/interfaces/interfaces';
   styleUrls: ['./cards.component.css']
 })
 export class CardsComponent {
-  @Input() orderOne: any = {};
+  @Input() orderOne!: any;
+  stop: boolean = false;
+  time: string = '';
 
-  constructor(){
-  console.log(this.orderOne);
+  constructor() {
+    console.log(this.orderOne);
   }
   public buttonState: boolean = false;
 
   public toggleButtonState(): void {
     this.buttonState = !this.buttonState;
+    this.stop = !this.stop;
   }
-
+  setTime(time: string):void {
+    //checkar para corregir la warra
+    if (time.length > 0 && time != 'NaN:NaN:NaN') {
+      this.time = time;
+    }
+    console.log(time, 'TIMEEEEE');
+  }
 }
