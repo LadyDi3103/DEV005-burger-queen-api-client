04d767cb9b984973596713bfb7822207a47218b8
LadyDi3103 <chinamy2004@gmail.com>
2023-07-21 15:07:04 -0500

    ADD: dist file

--- Burger-Queen-Api-Client/src/main.ts
+++ Burger-Queen-Api-Client/src/main.ts
@@ -1,6 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+
 // se ingresó este código para corregir error sel serviceWorker
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/ngsw-worker.js')
